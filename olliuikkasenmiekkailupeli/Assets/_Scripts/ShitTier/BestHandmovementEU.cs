fa45170d30d4b8cd71edcafe1cd73ebc897642c5
maukii <31925951+maukii@users.noreply.github.com>
2018-06-20 14:36:12 +0300

    handMovement - proto 2.58

---
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/ShitTier/BestHandmovementEU.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BestHandmovementEU : MonoBehaviour
+{
+    public float speed = 1f;
+    public float power = 1;
+    public GameObject hand, visualizer;
+
+    Vector3 direction;
+    Vector3 handStartPosition;
+
+    [Header("--- Input values ---")]
+    public float hor;
+    public float ver;
+
+    [Header("--- Maximum distances ---")]
+    public float maxH;
+    public float minH;
+    public float maxV;
+    public float minV;
+
+    void Start()
+    {
+        CheckPositions(); // TODO: if player takes step forward need to update hands start position and boundries
+        CheckBoundries();
+    }
+
+    void Update()
+    {
+        TakeInput(); // TODO: disable parts of input while player is moving
+        MoveHand();
+        ClampTargetPosition();
+    }
+
+    void CheckPositions()
+    {
+        handStartPosition = hand.transform.position;
+        visualizer.transform.position = handStartPosition;
+    }
+
+    void CheckBoundries()
+    {
+        maxH += handStartPosition.x;      // TODO: Update after player has moved,
+        minH -= -handStartPosition.x;     //       create animation event or ?? 
+        maxV += handStartPosition.y;      
+        minV -= -handStartPosition.y;     
+    }
+
+    void TakeInput()
+    {
+        hor = Input.GetAxisRaw("Horizontal");
+        ver = Input.GetAxisRaw("Vertical");
+        direction = new Vector3(hor, ver, 0).normalized;
+    }
+
+    void ClampTargetPosition()
+    {
+        Vector3 clampPos = visualizer.transform.position;
+        clampPos.y = Mathf.Clamp(visualizer.transform.position.y, minV, maxV);
+        clampPos.x = Mathf.Clamp(visualizer.transform.position.x, minH, maxH);
+        visualizer.transform.position = clampPos;
+    }
+
+    void MoveHand()
+    {
+        hand.GetComponent<Rigidbody>().AddForce((visualizer.transform.position - hand.transform.position) * 10000 * power);
+        visualizer.transform.Translate(direction * speed * Time.deltaTime);
+
+        if (hor == 0 && ver == 0)
+        {
+            visualizer.transform.position = Vector3.MoveTowards(visualizer.transform.position, handStartPosition, speed * Time.deltaTime);
+        }
+    }
+
+}

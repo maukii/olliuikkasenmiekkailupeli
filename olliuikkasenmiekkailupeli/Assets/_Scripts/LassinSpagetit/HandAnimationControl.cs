dab43e2e9d73de7449c40fbc057263cf2458afa9
BugiNinja <lassitormikoski@kamk.fi>
2018-06-26 15:23:16 +0300

    Lassin animaatio proto

---
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/LassinSpagetit/HandAnimationControl.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimationControl : MonoBehaviour {
+    float hanging;
+    float inside;
+    float strong;
+    public bool Inputframe;
+    public bool swordSwinging;
+    public float AnimSpeed = 0.5f;
+    public float InputAnimSpeed = 0.5f;
+    bool vitunTriggeritL = false;
+    bool vitunTriggeritR = false;
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("L1") && !swordSwinging)
+        {
+            SwapInside();
+            
+        }
+        if (Input.GetButtonDown("R1") && !swordSwinging)
+        {
+
+            SwapHanging();
+        }
+        
+        if (Input.GetAxis("R2") != 0 && !swordSwinging && !vitunTriggeritR)
+        {
+            vitunTriggeritR = true;
+            Swing();
+        }
+        else if(Input.GetAxis("R2") == 0 && Inputframe && swordSwinging && vitunTriggeritR)
+        {
+            vitunTriggeritR = false;
+            Weak();
+        }
+        if (Inputframe && anim.GetBool("SwingDia"))
+        {
+            SwapInside();
+            SwapHanging();
+            anim.SetBool("SwingDia", false);
+        }
+        if (Input.GetAxis("L2") != 0 && !swordSwinging && !vitunTriggeritL)
+        {
+            
+            vitunTriggeritL = true;
+            SwingHor();
+        }
+        else if (Input.GetAxis("L2") == 0 && Inputframe && swordSwinging && vitunTriggeritL)
+        {
+            vitunTriggeritL = false;
+            WeakHor();
+        }
+        if (Inputframe && anim.GetBool("SwingHor"))
+        {
+            SwapInside();
+            anim.SetBool("SwingHor", false);
+        }
+        if (Inputframe)
+        {
+            anim.speed = InputAnimSpeed;
+        }
+        else
+        {
+            anim.speed = AnimSpeed;
+        }
+        if (Input.GetAxis("L2") == 0)
+        {
+            vitunTriggeritL = false;
+        }
+        if (Input.GetAxis("R2") == 0)
+        {
+            vitunTriggeritR = false;
+        }
+    }
+    void Swing()
+    {
+        
+        anim.SetFloat("Strong", 1);
+        anim.SetBool("SwingDia", true);
+        
+    }
+    void Weak()
+    {
+        anim.SetFloat("Strong", 0);
+        SwapInside();
+        SwapHanging();
+    }
+    void SwingHor()
+    {
+
+        anim.SetFloat("Strong", 1);
+        anim.SetBool("SwingHor", true);
+
+    }
+    void WeakHor()
+    {
+        anim.SetFloat("Strong", 0);
+        SwapInside();
+    }
+    void SwapHanging()
+    {
+        if(hanging == 0)
+        {
+            hanging = 1;
+            anim.SetFloat("Hanging", 1);
+        }
+        else
+        {
+            hanging = 0;
+            anim.SetFloat("Hanging", 0);
+        }
+    }
+    void SwapInside()
+    {
+        if (inside == 0)
+        {
+            inside = 1;
+            anim.SetFloat("Inside", 1);
+        }
+        else
+        {
+            inside = 0;
+            anim.SetFloat("Inside", 0);
+        }
+    }
+    void SetInside(float value)
+    {
+        inside = value;
+        anim.SetFloat("Inside", value);
+    }
+    void SetHanging(float value)
+    {
+        hanging = value;
+        anim.SetFloat("Hanging", value);
+    }
+    float GetInside()
+    {
+        inside = anim.GetFloat("Inside");
+        return inside;
+    }
+    float GetHanging()
+    {
+        hanging = anim.GetFloat("Hanging");
+        return inside;
+    }
+}
--- olliuikkasenmiekkailupeli/Assets/_Scripts/LassinSpagetit/HandMove.cs
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/LassinSpagetit/HandMove.cs
@@ -10,9 +10,12 @@ public class HandMove : MonoBehaviour {
     Transform hanging;
     Transform thrust;
     Transform down;
+    public Transform windUp1;
+    Transform slash1;
     GameObject Inside;
     public Transform Outside;
-    public float progress = 0;
+    public float swapProgress = 0;
+    public float swingProgress = 0;
     public float Speed = 1;
     public bool isInside;
     public bool swap;
@@ -20,8 +23,17 @@ public class HandMove : MonoBehaviour {
     Quaternion downSwapRot;
     Vector3 hangingSwapPos;
     Vector3 downSwapPos;
+    enum Stance {Hanging, Default, Down}
+    Stance stance;
+    bool swing;
+    public bool altInput;
+    public bool otherAltInput;
+    float altInputvalue = 0;
+    float altInputSpeed;
     // Use this for initialization
     void Start () {
+        swing = false;
+        stance = Stance.Default;
         isInside = true;
         thrust = GameObject.Find("thrust").transform;
         Inside = GameObject.Find("inside");
@@ -32,68 +44,100 @@ public class HandMove : MonoBehaviour {
         defaultPosition = transform.localPosition;
         hanging = GameObject.Find("hanging").transform;
         down = GameObject.Find("down").transform;
+        windUp1 = GameObject.Find("windup1").transform;
+        slash1 = GameObject.Find("slash1").transform;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetAxis("Vertical") >= 0)
+        if (swing)
         {
-            transform.localRotation = Quaternion.Slerp(defaultRotation, hanging.localRotation, Input.GetAxis("Vertical"));
-            transform.localPosition = Vector3.Lerp(defaultPosition, hanging.localPosition, Input.GetAxis("Vertical"));
+            SwordSwing();
         }
-        else if (Input.GetAxis("Vertical") <= 0)
+        else
         {
-            transform.localRotation = Quaternion.Slerp(defaultRotation, down.localRotation, -Input.GetAxis("Vertical"));
-            transform.localPosition = Vector3.Lerp(defaultPosition, down.localPosition, -Input.GetAxis("Vertical"));
-        }
-        //if(Input.GetAxis("Horizontal") >= 0)
-        //{
-        //    transform.localRotation = Quaternion.Slerp(defaultRotation, thrust.localRotation, Input.GetAxis("Horizontal"));
-        //    transform.localPosition = Vector3.Lerp(defaultPosition, thrust.localPosition, Input.GetAxis("Horizontal"));
-        //}
-        if (Input.GetButton("Fire1") && progress == 0)
-        {
-            Swap();
-            hangingSwapRot = hanging.localRotation;
-            downSwapRot = down.localRotation;
-            hangingSwapPos = hanging.localPosition;
-            downSwapPos = down.localPosition;
-        }
-        if(swap == true)
-        {
-            progress = progress + Time.deltaTime * Speed;
-            if (!isInside)
-            {
-                Inside.transform.localRotation = Quaternion.Slerp(insideRotation, Outside.localRotation, progress);
-                Inside.transform.localPosition = Vector3.Lerp(insidePosition, Outside.localPosition, progress);
-                hanging.localRotation = Quaternion.Slerp(hangingSwapRot, Quaternion.Euler(-hangingSwapRot.eulerAngles.x, hangingSwapRot.eulerAngles.y, hangingSwapRot.eulerAngles.z), progress);
-                down.localRotation = Quaternion.Slerp(downSwapRot, Quaternion.Euler(-downSwapRot.eulerAngles.x, downSwapRot.eulerAngles.y, downSwapRot.eulerAngles.z), progress);
-                hanging.localPosition = Vector3.Lerp(hangingSwapPos, new Vector3(hangingSwapPos.x, -hangingSwapPos.y, -hangingSwapPos.z), progress);
-                down.localPosition = Vector3.Lerp(downSwapPos, new Vector3(downSwapPos.x, -downSwapPos.y, -downSwapPos.z), progress);
 
+            if (altInput)
+            {
+                AltUpdateStance();
+            }
+            else if (otherAltInput)
+            {
+                OtherAltUpdateStance();
             }
             else
             {
-                Inside.transform.localRotation = Quaternion.Slerp(Outside.localRotation, insideRotation, progress);
-                Inside.transform.localPosition = Vector3.Lerp(Outside.localPosition, insidePosition, progress);
-                hanging.localRotation = Quaternion.Slerp(hangingSwapRot, Quaternion.Euler(-hangingSwapRot.eulerAngles.x, hangingSwapRot.eulerAngles.y, hangingSwapRot.eulerAngles.z), progress);
-                down.localRotation = Quaternion.Slerp(downSwapRot, Quaternion.Euler(-downSwapRot.eulerAngles.x, downSwapRot.eulerAngles.y, downSwapRot.eulerAngles.z), progress);
-                hanging.localPosition = Vector3.Lerp(hangingSwapPos, new Vector3(hangingSwapPos.x, -hangingSwapPos.y, -hangingSwapPos.z), progress);
-                down.localPosition = Vector3.Lerp(downSwapPos, new Vector3(downSwapPos.x, -downSwapPos.y, -downSwapPos.z), progress);
-            }
-            Debug.Log(progress);
-            if (progress >= 1)
-            {
-                progress = 0;
-                swap = false;
+                UpdateStance();
             }
             
-        }
 
+            if (Input.GetButton("Fire2"))
+            {
+                swing = true;
+                Swap();
+                swap = false;
+                hangingSwapRot = hanging.localRotation;
+                downSwapRot = down.localRotation;
+                hangingSwapPos = hanging.localPosition;
+                downSwapPos = down.localPosition;
+            }
+
+
+            if (Input.GetButton("Fire1") && swapProgress == 0)
+            {
+                Swap();
+                hangingSwapRot = hanging.localRotation;
+                downSwapRot = down.localRotation;
+                hangingSwapPos = hanging.localPosition;
+                downSwapPos = down.localPosition;
+            }
+            if (swap == true)
+            {
+                swapProgress = swapProgress + Time.deltaTime * Speed;
+                if (!isInside)
+                {
+                    if(stance == Stance.Default)
+                    {
+
+                    }
+                    else if(stance == Stance.Hanging)
+                    {
+
+                    }
+                    else if (stance == Stance.Down)
+                    {
+
+                    }
+                    Inside.transform.localRotation = Quaternion.Slerp(insideRotation, Outside.localRotation, swapProgress);
+                    Inside.transform.localPosition = Vector3.Lerp(insidePosition, Outside.localPosition, swapProgress);
+                    hanging.localRotation = Quaternion.Slerp(hangingSwapRot, Quaternion.Euler(-hangingSwapRot.eulerAngles.x, hangingSwapRot.eulerAngles.y, hangingSwapRot.eulerAngles.z), swapProgress);
+                    down.localRotation = Quaternion.Slerp(downSwapRot, Quaternion.Euler(-downSwapRot.eulerAngles.x, downSwapRot.eulerAngles.y, downSwapRot.eulerAngles.z), swapProgress);
+                    hanging.localPosition = Vector3.Lerp(hangingSwapPos, new Vector3(hangingSwapPos.x, -hangingSwapPos.y, -hangingSwapPos.z), swapProgress);
+                    down.localPosition = Vector3.Lerp(downSwapPos, new Vector3(downSwapPos.x, -downSwapPos.y, -downSwapPos.z), swapProgress);
+
+                }
+                else
+                {
+                    Inside.transform.localRotation = Quaternion.Slerp(Outside.localRotation, insideRotation, swapProgress);
+                    Inside.transform.localPosition = Vector3.Lerp(Outside.localPosition, insidePosition, swapProgress);
+                    hanging.localRotation = Quaternion.Slerp(hangingSwapRot, Quaternion.Euler(-hangingSwapRot.eulerAngles.x, hangingSwapRot.eulerAngles.y, hangingSwapRot.eulerAngles.z), swapProgress);
+                    down.localRotation = Quaternion.Slerp(downSwapRot, Quaternion.Euler(-downSwapRot.eulerAngles.x, downSwapRot.eulerAngles.y, downSwapRot.eulerAngles.z), swapProgress);
+                    hanging.localPosition = Vector3.Lerp(hangingSwapPos, new Vector3(hangingSwapPos.x, -hangingSwapPos.y, -hangingSwapPos.z), swapProgress);
+                    down.localPosition = Vector3.Lerp(downSwapPos, new Vector3(downSwapPos.x, -downSwapPos.y, -downSwapPos.z), swapProgress);
+                }
+                if (swapProgress >= 1)
+                {
+                    swapProgress = 0;
+                    swap = false;
+                }
+
+            }
+        }
 
     }
     void Swap()
     {
+
         if(isInside == true)
         {
             isInside = false;
@@ -104,5 +148,168 @@ public class HandMove : MonoBehaviour {
             isInside = true;
             swap = true;
         }
+    }
+    void SwordSwing()
+    {
+        swingProgress = swingProgress + Time.deltaTime * Speed;
+        if (!isInside)
+        {
+
+            //transform.localPosition = Vector3.Lerp(hanging.localPosition, down.localPosition, progress-1);
+            //transform.localRotation = Quaternion.Slerp(hanging.localRotation, down.localRotation, progress-1);
+            
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, windUp1.localRotation, swingProgress);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, windUp1.localPosition, swingProgress);
+            if (swingProgress > 1)
+            {
+                Swap();
+                
+                
+            }
+            //hanging.localRotation = Quaternion.Slerp(hangingSwapRot, Quaternion.Euler(-hangingSwapRot.eulerAngles.x, hangingSwapRot.eulerAngles.y, hangingSwapRot.eulerAngles.z), progress-1);
+            //down.localRotation = Quaternion.Slerp(downSwapRot, Quaternion.Euler(-downSwapRot.eulerAngles.x, downSwapRot.eulerAngles.y, downSwapRot.eulerAngles.z), progress-1);
+            //hanging.localPosition = Vector3.Lerp(hangingSwapPos, new Vector3(hangingSwapPos.x, -hangingSwapPos.y, -hangingSwapPos.z), progress-1);
+            //down.localPosition = Vector3.Lerp(downSwapPos, new Vector3(downSwapPos.x, -downSwapPos.y, -downSwapPos.z), progress-1);
+
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Slerp(windUp1.localRotation, slash1.localRotation, swingProgress - 1);
+            transform.localPosition = Vector3.Lerp(windUp1.localPosition, slash1.localPosition, swingProgress - 1);
+        }
+        if (swingProgress >= 2)
+        {
+            swingProgress = 0;
+            swing = false;
+            if (otherAltInput)
+            {
+                altInputvalue = -1;
+            }
+        }
+    }
+    void SwordRotation()
+    {
+
+    }
+    void UpdateStance()
+    {
+        Quaternion verRot = Quaternion.identity, horRot = Quaternion.identity;
+        Vector3 verPos = Vector3.zero, horPos = Vector3.zero;
+        if (Input.GetAxis("Vertical") >= 0)
+        {
+            stance = Stance.Hanging;
+            verRot = Quaternion.Slerp(defaultRotation, hanging.localRotation, Input.GetAxis("Vertical"));
+            verPos = Vector3.Lerp(defaultPosition, hanging.localPosition, Input.GetAxis("Vertical"));
+        }
+        else if (Input.GetAxis("Vertical") <= 0)
+        {
+            stance = Stance.Down;
+            verRot = Quaternion.Slerp(defaultRotation, down.localRotation, -Input.GetAxis("Vertical"));
+            verPos = Vector3.Lerp(defaultPosition, down.localPosition, -Input.GetAxis("Vertical"));
+        }
+        if (Input.GetAxis("Horizontal") >= 0)
+        {
+            horRot = Quaternion.Slerp(defaultRotation, thrust.localRotation, Input.GetAxis("Horizontal"));
+            horPos = Vector3.Lerp(defaultPosition, thrust.localPosition, Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            stance = Stance.Default;
+        }
+        //transform.localRotation = Quaternion.Slerp(verRot, horRot, 0.5f);
+        //transform.localPosition = Vector3.Lerp(verPos*2, horPos*2, 0.5f);
+        transform.localRotation = verRot;
+        transform.localPosition = verPos;
+    }
+    void AltUpdateStance()
+    {
+        Quaternion verRot = Quaternion.identity, horRot = Quaternion.identity;
+        Vector3 verPos = Vector3.zero, horPos = Vector3.zero;
+        if(Input.GetAxis("Vertical") != 0)
+        {
+            altInputvalue += altInputSpeed * Input.GetAxis("Vertical");
+        }
+        if(altInputvalue > 1)
+        {
+            altInputvalue = 1;
+        }
+        else if(altInputvalue < -1)
+        {
+            altInputvalue = -1;
+        }
+        if (altInputvalue > 0)
+        {
+            stance = Stance.Hanging;
+            verRot = Quaternion.Slerp(defaultRotation, hanging.localRotation, altInputvalue);
+            verPos = Vector3.Lerp(defaultPosition, hanging.localPosition, altInputvalue);
+        }
+        else if (altInputvalue < 0)
+        {
+            stance = Stance.Down;
+            verRot = Quaternion.Slerp(defaultRotation, down.localRotation, -altInputvalue);
+            verPos = Vector3.Lerp(defaultPosition, down.localPosition, -altInputvalue);
+        }
+        if (Input.GetAxis("Horizontal") >= 0)
+        {
+            horRot = Quaternion.Slerp(defaultRotation, thrust.localRotation, Input.GetAxis("Horizontal"));
+            horPos = Vector3.Lerp(defaultPosition, thrust.localPosition, Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            stance = Stance.Default;
+        }
+        
+        //transform.localRotation = Quaternion.Slerp(verRot, horRot, 0.5f);
+        //transform.localPosition = Vector3.Lerp(verPos*2, horPos*2, 0.5f);
+        transform.localRotation = verRot;
+        transform.localPosition = verPos;
+    }
+    void OtherAltUpdateStance()
+    {
+        Quaternion verRot = Quaternion.identity, horRot = Quaternion.identity;
+        Vector3 verPos = Vector3.zero, horPos = Vector3.zero;
+        float smooth;
+        if (Input.GetButton("Fire3"))
+        {
+            altInputvalue += 1;
+        }
+        if (Input.GetButton("Jump"))
+        {
+            altInputvalue += -1;
+        }
+        if (altInputvalue > 1)
+        {
+            altInputvalue = 1;
+        }
+        else if (altInputvalue < -1)
+        {
+            altInputvalue = -1;
+        }
+        if (altInputvalue > 0)
+        {
+            stance = Stance.Hanging;
+            verRot = Quaternion.Slerp(defaultRotation, hanging.localRotation, altInputvalue);
+            verPos = Vector3.Lerp(defaultPosition, hanging.localPosition, altInputvalue);
+        }
+        else if (altInputvalue < 0)
+        {
+            stance = Stance.Down;
+            verRot = Quaternion.Slerp(defaultRotation, down.localRotation, -altInputvalue);
+            verPos = Vector3.Lerp(defaultPosition, down.localPosition, -altInputvalue);
+        }
+        if (Input.GetAxis("Horizontal") >= 0)
+        {
+            horRot = Quaternion.Slerp(defaultRotation, thrust.localRotation, Input.GetAxis("Horizontal"));
+            horPos = Vector3.Lerp(defaultPosition, thrust.localPosition, Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            stance = Stance.Default;
+        }
+
+        //transform.localRotation = Quaternion.Slerp(verRot, horRot, 0.5f);
+        //transform.localPosition = Vector3.Lerp(verPos*2, horPos*2, 0.5f);
+        transform.localRotation = verRot;
+        transform.localPosition = verPos;
     }
 }

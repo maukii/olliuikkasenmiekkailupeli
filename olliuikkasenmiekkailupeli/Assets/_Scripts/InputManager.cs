0888e89728a035f669ff1d44b876e2b2ab073687
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2018-06-29 12:31:25 +0300

    InputManager stuff part 4

--- olliuikkasenmiekkailupeli/Assets/_Scripts/InputManager.cs
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/InputManager.cs
@@ -4,8 +4,46 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    public float P1_LS_X, P1_LS_Y, P1_RS_X, P1_RS_Y, P2_LS_X, P2_LS_Y, P2_RS_X, P2_RS_Y;
-    public bool isXboxControllerP1, isXboxControllerP2, isPSControllerP1, isPSControllerP2;
+    [Header("Player 1")]
+    public float P1_LS_X;
+    public float P1_LS_Y;
+    public float P1_RS_X;
+    public float P1_RS_Y;
+    public float P1_Dpad_X;
+    public float P1_Dpad_Y;
+    public float P1_LT;
+    public float P1_RT;
+    public bool P1_LB;
+    public bool P1_RB;
+    public bool P1_A;
+    public bool P1_B;
+    public bool P1_X;
+    public bool P1_Y;
+    public bool P1_Start;
+
+    [Header("Player 2")]
+    public float P2_LS_X;
+    public float P2_LS_Y;
+    public float P2_RS_X;
+    public float P2_RS_Y;
+    public float P2_Dpad_X;
+    public float P2_Dpad_Y;
+    public float P2_LT;
+    public float P2_RT;
+    public bool P2_LB;
+    public bool P2_RB;
+    public bool P2_A;
+    public bool P2_B;
+    public bool P2_X;
+    public bool P2_Y;
+    public bool P2_Start;
+
+    [Header("Controller Check")]
+    public bool isXboxControllerP1;
+    public bool isXboxControllerP2;
+    public bool isPSControllerP1;
+    public bool isPSControllerP2;
+
     public static InputManager IM;
 
     void Start()
@@ -87,6 +125,17 @@ public class InputManager : MonoBehaviour
             P1_LS_Y = Input.GetAxis("Xbox_P1_VerticalLeft");
             P1_RS_X = Input.GetAxis("Xbox_P1_HorizontalRight");
             P1_RS_Y = Input.GetAxis("Xbox_P1_VerticalRight");
+            P1_Dpad_X = Input.GetAxis("Xbox_P1_HorizontalDpad");
+            P1_Dpad_Y = Input.GetAxis("Xbox_P1_VerticalDpad");
+            P1_LT = Input.GetAxis("Xbox_P1_LT");
+            P1_RT = Input.GetAxis("Xbox_P1_RT");
+            P1_LB = Input.GetKey(KeyCode.Joystick1Button4);
+            P1_RB = Input.GetKey(KeyCode.Joystick1Button5);
+            P1_A = Input.GetKey(KeyCode.Joystick1Button0);
+            P1_B = Input.GetKey(KeyCode.Joystick1Button1);
+            P1_X = Input.GetKey(KeyCode.Joystick1Button2);
+            P1_Y = Input.GetKey(KeyCode.Joystick1Button3);
+            P1_Start = Input.GetKey(KeyCode.Joystick1Button7);
 
             /*
             LS X axis = X axis          Input.GetAxis("Xbox_P1_HorizontalLeft")
@@ -118,6 +167,17 @@ public class InputManager : MonoBehaviour
             P2_LS_Y = Input.GetAxis("Xbox_P2_VerticalLeft");
             P2_RS_X = Input.GetAxis("Xbox_P2_HorizontalRight");
             P2_RS_Y = Input.GetAxis("Xbox_P2_VerticalRight");
+            P2_Dpad_X = Input.GetAxis("Xbox_P2_HorizontalDpad");
+            P2_Dpad_Y = Input.GetAxis("Xbox_P2_VerticalDpad");
+            P2_LT = Input.GetAxis("Xbox_P2_LT");
+            P2_RT = Input.GetAxis("Xbox_P2_RT");
+            P2_LB = Input.GetKey(KeyCode.Joystick2Button4);
+            P2_RB = Input.GetKey(KeyCode.Joystick2Button5);
+            P2_A = Input.GetKey(KeyCode.Joystick2Button0);
+            P2_B = Input.GetKey(KeyCode.Joystick2Button1);
+            P2_X = Input.GetKey(KeyCode.Joystick2Button2);
+            P2_Y = Input.GetKey(KeyCode.Joystick2Button3);
+            P2_Start = Input.GetKey(KeyCode.Joystick2Button7);
 
             /*
             LS X axis = X axis          Input.GetAxis("Xbox_P2_HorizontalLeft")
@@ -149,6 +209,17 @@ public class InputManager : MonoBehaviour
             P1_LS_Y = Input.GetAxis("PS_P1_VerticalLeft");
             P1_RS_X = Input.GetAxis("PS_P1_HorizontalRight");
             P1_RS_Y = Input.GetAxis("PS_P1_VerticalRight");
+            P1_Dpad_X = Input.GetAxis("PS_P1_HorizontalDpad");
+            P1_Dpad_Y = Input.GetAxis("PS_P1_VerticalDpad");
+            P1_LT = Input.GetAxis("PS_P1_L2");
+            P1_RT = Input.GetAxis("PS_P1_R2");
+            P1_LB = Input.GetKey(KeyCode.Joystick1Button4);
+            P1_RB = Input.GetKey(KeyCode.Joystick1Button5);
+            P1_A = Input.GetKey(KeyCode.Joystick1Button1);
+            P1_B = Input.GetKey(KeyCode.Joystick1Button2);
+            P1_X = Input.GetKey(KeyCode.Joystick1Button0);
+            P1_Y = Input.GetKey(KeyCode.Joystick1Button3);
+            P1_Start = Input.GetKey(KeyCode.Joystick1Button9);
 
             /*
             LS X axis = X axis                                          Input.GetAxis("PS_P1_HorizontalLeft")
@@ -156,7 +227,7 @@ public class InputManager : MonoBehaviour
             RS X axis = 3rd axis                                        Input.GetAxis("PS_P1_HorizontalRight")
             RS Y axis = 6th axis                                        Input.GetAxis("PS_P1_VerticalRight")
             D-pad X axis = 7th axis                                     Input.GetAxis("PS_P1_HorizontalDpad")
-            D-pad Y axis = 9th axis                                     Input.GetAxis("PS_P1_VerticalDpad")
+            D-pad Y axis = 8th axis                                     Input.GetAxis("PS_P1_VerticalDpad")
             L2 = 4th axis (-1.0f to 1.0f range, unpressed is -1.0f)     Input.GetAxis("PS_P1_L2")
             R2 = 5th axis (-1.0f to 1.0f range, unpressed is -1.0f)     Input.GetAxis("PS_P1_R2")
 
@@ -183,6 +254,17 @@ public class InputManager : MonoBehaviour
             P2_LS_Y = Input.GetAxis("PS_P2_VerticalLeft");
             P2_RS_X = Input.GetAxis("PS_P2_HorizontalRight");
             P2_RS_Y = Input.GetAxis("PS_P2_VerticalRight");
+            P2_Dpad_X = Input.GetAxis("PS_P2_HorizontalDpad");
+            P2_Dpad_Y = Input.GetAxis("PS_P2_VerticalDpad");
+            P2_LT = Input.GetAxis("PS_P2_L2");
+            P2_RT = Input.GetAxis("PS_P2_R2");
+            P2_LB = Input.GetKey(KeyCode.Joystick2Button4);
+            P2_RB = Input.GetKey(KeyCode.Joystick2Button5);
+            P2_A = Input.GetKey(KeyCode.Joystick2Button1);
+            P2_B = Input.GetKey(KeyCode.Joystick2Button2);
+            P2_X = Input.GetKey(KeyCode.Joystick2Button0);
+            P2_Y = Input.GetKey(KeyCode.Joystick2Button3);
+            P2_Start = Input.GetKey(KeyCode.Joystick2Button9);
 
             /*
             LS X axis = X axis                                          Input.GetAxis("PS_P2_HorizontalLeft")
@@ -190,7 +272,7 @@ public class InputManager : MonoBehaviour
             RS X axis = 3rd axis                                        Input.GetAxis("PS_P2_HorizontalRight")
             RS Y axis = 6th axis                                        Input.GetAxis("PS_P2_VerticalRight")
             D-pad X axis = 7th axis                                     Input.GetAxis("PS_P2_HorizontalDpad")
-            D-pad Y axis = 9th axis                                     Input.GetAxis("PS_P2_VerticalDpad")
+            D-pad Y axis = 8th axis                                     Input.GetAxis("PS_P2_VerticalDpad")
             L2 = 4th axis (-1.0f to 1.0f range, unpressed is -1.0f)     Input.GetAxis("PS_P2_L2")
             R2 = 5th axis (-1.0f to 1.0f range, unpressed is -1.0f)     Input.GetAxis("PS_P2_R2")
 

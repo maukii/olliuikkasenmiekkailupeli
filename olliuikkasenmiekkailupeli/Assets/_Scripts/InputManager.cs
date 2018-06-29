0d72f1c40ac2d2fc4d3e571c095b25cefc87ae6b
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2018-06-29 10:13:38 +0300

    InputManager stuff part 3

--- olliuikkasenmiekkailupeli/Assets/_Scripts/InputManager.cs
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/InputManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    public float P1_LS_X, P1_LS_Y, P1_RS_X, P1_RS_Y, P2_LS_X, P2_LS_Y, P2_RS_X, P2_RS_Y;
     public bool isXboxControllerP1, isXboxControllerP2, isPSControllerP1, isPSControllerP2;
     public static InputManager IM;
 
@@ -19,6 +20,8 @@ public class InputManager : MonoBehaviour
         {
             ControllerCheck();
         }
+
+        ChooseInput();
     }
 
     void ControllerCheck()
@@ -33,7 +36,7 @@ public class InputManager : MonoBehaviour
                 isXboxControllerP1 = true;
             }
 
-            if (names[0].Contains("PS3") || names[0].Contains("PS4"))
+            if (names[0].Contains("PLAYSTATION") || names[0].Contains("Wireless Controller"))
             {
                 Debug.Log("PlayStation controller detected as joystick 1");
                 isPSControllerP1 = true;
@@ -45,75 +48,167 @@ public class InputManager : MonoBehaviour
                 isXboxControllerP2 = true;
             }
 
-            if (names[1].Contains("PS3") || names[1].Contains("PS4"))
+            if (names[1].Contains("PLAYSTATION") || names[1].Contains("Wireless Controller"))
             {
                 Debug.Log("PlayStation controller detected as joystick 2");
                 isPSControllerP2 = true;
             }
         }
 
-        if (names.Length == 0)
+        else
         {
             Debug.Log("No controllers found");
+            isXboxControllerP1 = false;
+            isXboxControllerP2 = false;
+            isPSControllerP1 = false;
+            isPSControllerP2 = false;
         }
 
         if (names[0].Length == 0)
         {
             Debug.Log("Can't find joystick 1");
+            isXboxControllerP1 = false;
+            isPSControllerP1 = false;
         }
 
         if (names[1].Length == 0)
         {
             Debug.Log("Can't find joystick 2");
+            isXboxControllerP2 = false;
+            isPSControllerP2 = false;
+        }
+    }
+
+    void ChooseInput()
+    {
+        if (isXboxControllerP1)
+        {
+            P1_LS_X = Input.GetAxis("Xbox_P1_HorizontalLeft");
+            P1_LS_Y = Input.GetAxis("Xbox_P1_VerticalLeft");
+            P1_RS_X = Input.GetAxis("Xbox_P1_HorizontalRight");
+            P1_RS_Y = Input.GetAxis("Xbox_P1_VerticalRight");
+
+            /*
+            LS X axis = X axis          Input.GetAxis("Xbox_P1_HorizontalLeft")
+            LS Y axis = Y axis          Input.GetAxis("Xbox_P1_VerticalLeft")
+            RS X axis = 4th axis        Input.GetAxis("Xbox_P1_HorizontalRight")
+            RS Y axis = 5th axis        Input.GetAxis("Xbox_P1_VerticalRight")
+            D-pad X axis = 6th axis     Input.GetAxis("Xbox_P1_HorizontalDpad")
+            D-pad Y axis = 7th axis     Input.GetAxis("Xbox_P1_VerticalDpad")
+            Triggers? = 3rd axis        Input.GetAxis("Xbox_P1_LT_RT")
+            LT = 9th axis               Input.GetAxis("Xbox_P1_LT")
+            RT = 10th axis              Input.GetAxis("Xbox_P1_RT")
+
+            A button = Button 0         Input.GetButton("Xbox_P1_A")
+            B button = Button 1         Input.GetButton("Xbox_P1_B")
+            X button = Button 2         Input.GetButton("Xbox_P1_X")
+            Y button = Button 3         Input.GetButton("Xbox_P1_Y")
+            LB = Button 4               Input.GetButton("Xbox_P1_LB")
+            RB = Button 5               Input.GetButton("Xbox_P1_RB")
+            Back button = Button 6      Input.GetButton("Xbox_P1_Back")
+            Start button = Button 7     Input.GetButton("Xbox_P1_Start")
+            LS = Button 8               Input.GetButton("Xbox_P1_LS")
+            RS = Button 9               Input.GetButton("Xbox_P1_RS")
+            */
+        }
+
+        if (isXboxControllerP2)
+        {
+            P2_LS_X = Input.GetAxis("Xbox_P2_HorizontalLeft");
+            P2_LS_Y = Input.GetAxis("Xbox_P2_VerticalLeft");
+            P2_RS_X = Input.GetAxis("Xbox_P2_HorizontalRight");
+            P2_RS_Y = Input.GetAxis("Xbox_P2_VerticalRight");
+
+            /*
+            LS X axis = X axis          Input.GetAxis("Xbox_P2_HorizontalLeft")
+            LS Y axis = Y axis          Input.GetAxis("Xbox_P2_VerticalLeft")
+            RS X axis = 4th axis        Input.GetAxis("Xbox_P2_HorizontalRight")
+            RS Y axis = 5th axis        Input.GetAxis("Xbox_P2_VerticalRight")
+            D-pad X axis = 6th axis     Input.GetAxis("Xbox_P2_HorizontalDpad")
+            D-pad Y axis = 7th axis     Input.GetAxis("Xbox_P2_VerticalDpad")
+            Triggers? = 3rd axis        Input.GetAxis("Xbox_P2_LT_RT")
+            LT = 9th axis               Input.GetAxis("Xbox_P2_LT")
+            RT = 10th axis              Input.GetAxis("Xbox_P2_RT")
+
+            A button = Button 0         Input.GetButton("Xbox_P2_A")
+            B button = Button 1         Input.GetButton("Xbox_P2_B")
+            X button = Button 2         Input.GetButton("Xbox_P2_X")
+            Y button = Button 3         Input.GetButton("Xbox_P2_Y")
+            LB = Button 4               Input.GetButton("Xbox_P2_LB")
+            RB = Button 5               Input.GetButton("Xbox_P2_RB")
+            Back button = Button 6      Input.GetButton("Xbox_P2_Back")
+            Start button = Button 7     Input.GetButton("Xbox_P2_Start")
+            LS = Button 8               Input.GetButton("Xbox_P2_LS")
+            RS = Button 9               Input.GetButton("Xbox_P2_RS")
+            */
+        }
+
+        if (isPSControllerP1)
+        {
+            P1_LS_X = Input.GetAxis("PS_P1_HorizontalLeft");
+            P1_LS_Y = Input.GetAxis("PS_P1_VerticalLeft");
+            P1_RS_X = Input.GetAxis("PS_P1_HorizontalRight");
+            P1_RS_Y = Input.GetAxis("PS_P1_VerticalRight");
+
+            /*
+            LS X axis = X axis                                          Input.GetAxis("PS_P1_HorizontalLeft")
+            LS Y axis = Y axis                                          Input.GetAxis("PS_P1_VerticalLeft")
+            RS X axis = 3rd axis                                        Input.GetAxis("PS_P1_HorizontalRight")
+            RS Y axis = 6th axis                                        Input.GetAxis("PS_P1_VerticalRight")
+            D-pad X axis = 7th axis                                     Input.GetAxis("PS_P1_HorizontalDpad")
+            D-pad Y axis = 9th axis                                     Input.GetAxis("PS_P1_VerticalDpad")
+            L2 = 4th axis (-1.0f to 1.0f range, unpressed is -1.0f)     Input.GetAxis("PS_P1_L2")
+            R2 = 5th axis (-1.0f to 1.0f range, unpressed is -1.0f)     Input.GetAxis("PS_P1_R2")
+
+            Square = Button 0                                           Input.GetButton("PS_P1_Square")
+            X = Button 1                                                Input.GetButton("PS_P1_X")
+            Circle = Button 2                                           Input.GetButton("PS_P1_Circle")
+            Triangle = Button 3                                         Input.GetButton("PS_P1_Triangle")
+            L1 = Button 4                                               Input.GetButton("PS_P1_L1")
+            R1 = Button 5                                               Input.GetButton("PS_P1_R1")
+            L2 = Button 6                                               Input.GetButton("PS_P1_L2")
+            R2 = Button 7                                               Input.GetButton("PS_P1_R2")
+            Share = Button 8                                            Input.GetButton("PS_P1_Share")
+            Options = Button 9                                          Input.GetButton("PS_P1_Options")
+            L3 = Button 10                                              Input.GetButton("PS_P1_L3")
+            R3 = Button 11                                              Input.GetButton("PS_P1_R3")
+            PS = Button 12                                              Input.GetButton("PS_P1_PS")
+            PadPress = Button 13                                        Input.GetButton("PS_P1_PadPress")
+            */
+        }
+
+        if (isPSControllerP2)
+        {
+            P2_LS_X = Input.GetAxis("PS_P2_HorizontalLeft");
+            P2_LS_Y = Input.GetAxis("PS_P2_VerticalLeft");
+            P2_RS_X = Input.GetAxis("PS_P2_HorizontalRight");
+            P2_RS_Y = Input.GetAxis("PS_P2_VerticalRight");
+
+            /*
+            LS X axis = X axis                                          Input.GetAxis("PS_P2_HorizontalLeft")
+            LS Y axis = Y axis                                          Input.GetAxis("PS_P2_VerticalLeft")
+            RS X axis = 3rd axis                                        Input.GetAxis("PS_P2_HorizontalRight")
+            RS Y axis = 6th axis                                        Input.GetAxis("PS_P2_VerticalRight")
+            D-pad X axis = 7th axis                                     Input.GetAxis("PS_P2_HorizontalDpad")
+            D-pad Y axis = 9th axis                                     Input.GetAxis("PS_P2_VerticalDpad")
+            L2 = 4th axis (-1.0f to 1.0f range, unpressed is -1.0f)     Input.GetAxis("PS_P2_L2")
+            R2 = 5th axis (-1.0f to 1.0f range, unpressed is -1.0f)     Input.GetAxis("PS_P2_R2")
+
+            Square = Button 0                                           Input.GetButton("PS_P2_Square")
+            X = Button 1                                                Input.GetButton("PS_P2_X")
+            Circle = Button 2                                           Input.GetButton("PS_P2_Circle")
+            Triangle = Button 3                                         Input.GetButton("PS_P2_Triangle")
+            L1 = Button 4                                               Input.GetButton("PS_P2_L1")
+            R1 = Button 5                                               Input.GetButton("PS_P2_R1")
+            L2 = Button 6                                               Input.GetButton("PS_P2_L2")
+            R2 = Button 7                                               Input.GetButton("PS_P2_R2")
+            Share = Button 8                                            Input.GetButton("PS_P2_Share")
+            Options = Button 9                                          Input.GetButton("PS_P2_Options")
+            L3 = Button 10                                              Input.GetButton("PS_P2_L3")
+            R3 = Button 11                                              Input.GetButton("PS_P2_R3")
+            PS = Button 12                                              Input.GetButton("PS_P2_PS")
+            PadPress = Button 13                                        Input.GetButton("PS_P2_PadPress")
+            */
         }
     }
 }
-
-/*
-        ----- Xbox -----
-        LS X axis = X axis
-        LS Y axis = Y axis
-        RS X axis = 4th axis
-        RS Y axis = 5th axis
-        D-pad X axis = 6th axis
-        D-pad Y axis = 7th axis
-        Triggers? = 3rd axis
-        LT = 9th axis
-        RT = 10th axis
-
-        A button = Button 0
-        B button = Button 1
-        X button = Button 2
-        Y button = Button 3
-        LB = Button 4
-        RB = Button 5
-        Back button = Button 6
-        Start button = Button 7
-        LS = Button 8
-        RS = Button 9
-    
-        ----- PS4 -----
-        LS X axis = X axis
-        LS Y axis = Y axis (inverted?)
-        RS X axis = 3rd axis
-        RS Y axis = 4th axis
-        D-pad X axis = 7th axis
-        D-pad Y axis = 8th axis (inverted?)
-        L2 = 5th axis (-1.0f to 1.0f range, unpressed is -1.0f)
-        R2 = 6th axis (-1.0f to 1.0f range, unpressed is -1.0f)
-
-        Square = Button 0
-        X = Button 1
-        Circle = Button 2
-        Triangle = Button 3
-        L1 = Button 4
-        R1 = Button 5
-        L2 = Button 6
-        R2 = Button 7
-        Share = Button 8
-        Options = Button 9
-        L3 = Button 10
-        R3 = Button 11
-        PS = Button 12
-        PadPress = Button 13
-    */

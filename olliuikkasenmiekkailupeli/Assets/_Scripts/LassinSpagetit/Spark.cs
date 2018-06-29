8ee7157cd480690e305480d39831b613f203fa7a
BugiNinja <lassitormikoski@kamk.fi>
2018-06-29 12:31:58 +0300

    Lassintesti collision update

---
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/LassinSpagetit/Spark.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spark : MonoBehaviour {
+    Light pl;
+    public float intensity;
+    public float FadeSpeed = 15;
+	// Use this for initialization
+	void Start () {
+        pl = gameObject.GetComponent<Light>();
+        intensity = 10;
+        pl.intensity = intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        intensity = intensity - Time.deltaTime * FadeSpeed;
+        pl.intensity = intensity;
+        if(intensity <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}

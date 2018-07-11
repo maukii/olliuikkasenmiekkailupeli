955ca6ccfaa77f8c00d5cc89e8b9276cb242ed5b
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2018-07-11 13:17:56 +0300

    test main menu stuff again

--- olliuikkasenmiekkailupeli/Assets/_Scripts/MenuScheisse/TestMainMenuTitleBar.cs
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/MenuScheisse/TestMainMenuTitleBar.cs
@@ -5,6 +5,76 @@ using UnityEngine.UI;
 
 public class TestMainMenuTitleBar : MonoBehaviour
 {
+    public Image tb1, tb2, tb3;
+
+    void Start()
+    {
+        tb1.enabled = false;
+        tb2.enabled = false;
+        tb3.enabled = false;
+    }
+
+    void Update()
+    {
+        if (InputManager.IM.P1_LS_X < 0.01f && InputManager.IM.P1_LS_X >= -0.33f)
+        {
+            //Start Game
+            tb1.enabled = true;
+            tb2.enabled = false;
+        }
+
+        if (InputManager.IM.P1_LS_X < -0.33f && InputManager.IM.P1_LS_X >= -0.66f)
+        {
+            //Options
+            tb1.enabled = false;
+            tb2.enabled = true;
+            tb3.enabled = false;
+        }
+
+        if (InputManager.IM.P1_LS_X < -0.66f && InputManager.IM.P1_LS_X >= -1f)
+        {
+            //Exit
+            tb2.enabled = false;
+            tb3.enabled = true;
+        }
+    }
+
+    void Menu()
+    {
+        if (tb1.enabled && Input.GetKey(KeyCode.Space))
+        {
+            StartGame();
+        }
+
+        if (tb2.enabled && Input.GetKey(KeyCode.Space))
+        {
+            Options();
+        }
+
+        if (tb3.enabled && Input.GetKey(KeyCode.Space))
+        {
+            QuitGame();
+        }
+    }
+
+    void StartGame()
+    {
+        Debug.Log("START GAME");
+    }
+
+    void Options()
+    {
+        Debug.Log("OPTIONS");
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("EXIT");
+    }
+
+    /*
+    //Visual stuff only...
     //Tällä hetkellä Box Collider objektissa 'kämmen.L'
     public Image bar;
     public bool isTouching;
@@ -37,4 +107,5 @@ public class TestMainMenuTitleBar : MonoBehaviour
             isTouching = false;
         }
     }
+    */
 }

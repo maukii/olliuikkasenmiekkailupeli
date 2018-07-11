390bed3d1c9462dfc3508a10d814869c32428b6c
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2018-07-11 09:27:54 +0300

    test main menu + parallax test scene

---
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/MenuScheisse/TestMainMenuTitleBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestMainMenuTitleBar : MonoBehaviour
+{
+    //Tällä hetkellä Box Collider objektissa 'kämmen.R'
+    public Image bar;
+    public bool isTouching;
+
+    void Update()
+    {
+        if (isTouching)
+        {
+            bar.enabled = true;
+        }
+
+        else
+        {
+            bar.enabled = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Untagged")) //Laita joku toinen tagi tai sitten ei...
+        {
+            isTouching = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("Untagged"))
+        {
+            isTouching = false;
+        }
+    }
+}

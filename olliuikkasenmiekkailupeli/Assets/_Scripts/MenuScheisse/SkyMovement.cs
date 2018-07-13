aa61d673d90bbaf20d90b84e7c2f9857545e9ecc
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2018-07-13 13:46:56 +0300

    sky moves in menu

---
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/MenuScheisse/SkyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyMovement : MonoBehaviour
+{
+    public GameObject sky;
+
+    private Vector3 skyVec, skyNew;
+    private float startTime, length, distance, fraction;
+    private float speed = 1.5f;
+
+	void Start ()
+    {
+        skyVec = sky.transform.position;
+        skyNew = new Vector3(-74.5f, skyVec.y, skyVec.z);
+
+        startTime = Time.time;
+        length = Vector3.Distance(skyVec, skyNew);
+    }
+
+	void Update ()
+    {
+        distance = (Time.time - startTime) * speed;
+        fraction = distance / length;
+        sky.transform.position = Vector3.Lerp(skyVec, skyNew, fraction);
+    }
+}

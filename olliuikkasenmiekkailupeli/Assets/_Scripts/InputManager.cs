4f6a8c0f7dcb9acff5910d4148ced4a0edfbed1d
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2018-07-17 13:28:17 +0300

    F310 seems to work as xbox controller

--- olliuikkasenmiekkailupeli/Assets/_Scripts/InputManager.cs
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/InputManager.cs
@@ -94,7 +94,7 @@ public class InputManager : MonoBehaviour
 
         if (names.Length > 0)
         {
-            if (names[0].Contains("XBOX 360") || names[0].Contains("Xbox One"))
+            if (names[0].Contains("XBOX 360") || names[0].Contains("Xbox One") || names[0].Contains("Controller (Gamepad F310)"))
             {
                 Debug.LogWarning(names[0] + " detected as joystick 1");
                 isXboxControllerP1 = true;
@@ -108,16 +108,9 @@ public class InputManager : MonoBehaviour
                 isKeyboardAndMouseP1 = false;
             }
 
-            if (names[0].Contains("Controller (Gamepad F310)"))
-            {
-                Debug.LogWarning(names[0] + " detected as joystick 1");
-                isLogitechControllerP1 = true;
-                isKeyboardAndMouseP1 = false;
-            }
-
             if(names.Length > 1)
             {
-                if (names[1].Contains("XBOX 360") || names[1].Contains("Xbox One"))
+                if (names[1].Contains("XBOX 360") || names[1].Contains("Xbox One") || names[1].Contains("Controller (Gamepad F310)"))
                 {
                     Debug.LogWarning(names[1] + " detected as joystick 2");
                     isXboxControllerP2 = true;
@@ -129,13 +122,6 @@ public class InputManager : MonoBehaviour
                     Debug.LogWarning(names[1] + " detected as joystick 2");
                     isPSControllerP2 = true;
                     isKeyboardAndMouseP2 = false;
-                }
-
-                if (names[1].Contains("Controller (Gamepad F310)"))
-                {
-                    Debug.LogWarning(names[1] + " detected as joystick 2");
-                    isLogitechControllerP2 = true;
-                    isKeyboardAndMouseP1 = false;
                 }
 
                 if (names[0].Length == 0 && names[1].Length > 0)
@@ -264,48 +250,6 @@ public class InputManager : MonoBehaviour
             P2_X = Input.GetKey(KeyCode.Joystick2Button0);
             P2_Y = Input.GetKey(KeyCode.Joystick2Button3);
             P2_Start = Input.GetKey(KeyCode.Joystick2Button9);
-        }
-
-        if (isLogitechControllerP1)
-        {
-            P1_Hor = "PS_P1_HorizontalLeft";
-            P1_Ver = "PS_P1_VerticalLeft";
-            P1_LS_X = Input.GetAxis("PS_P1_HorizontalLeft");
-            P1_LS_Y = Input.GetAxis("PS_P1_VerticalLeft");
-            P1_RS_X = Input.GetAxis("Xbox_P1_HorizontalRight");
-            P1_RS_Y = Input.GetAxis("Xbox_P1_VerticalRight");
-            P1_Dpad_X = Input.GetAxis("Xbox_P1_HorizontalDpad");
-            P1_Dpad_Y = Input.GetAxis("Xbox_P1_VerticalDpad");
-            P1_LT = Input.GetAxis("Xbox_P1_LT");
-            P1_RT = Input.GetAxis("Xbox_P1_RT");
-            P1_LB = Input.GetKey(KeyCode.Joystick1Button4);
-            P1_RB = Input.GetKey(KeyCode.Joystick1Button5);
-            P1_A = Input.GetKey(KeyCode.Joystick1Button0);
-            P1_B = Input.GetKey(KeyCode.Joystick1Button1);
-            P1_X = Input.GetKey(KeyCode.Joystick1Button2);
-            P1_Y = Input.GetKey(KeyCode.Joystick1Button3);
-            P1_Start = Input.GetKey(KeyCode.Joystick1Button7);
-        }
-
-        if (isLogitechControllerP2)
-        {
-            P2_Hor = "PS_P2_HorizontalLeft";
-            P2_Ver = "PS_P2_VerticalLeft";
-            P2_LS_X = Input.GetAxis("PS_P2_HorizontalLeft");
-            P2_LS_Y = Input.GetAxis("PS_P2_VerticalLeft");
-            P2_RS_X = Input.GetAxis("Xbox_P2_HorizontalRight");
-            P2_RS_Y = Input.GetAxis("Xbox_P2_VerticalRight");
-            P2_Dpad_X = Input.GetAxis("Xbox_P2_HorizontalDpad");
-            P2_Dpad_Y = Input.GetAxis("Xbox_P2_VerticalDpad");
-            P2_LT = Input.GetAxis("Xbox_P2_LT");
-            P2_RT = Input.GetAxis("Xbox_P2_RT");
-            P2_LB = Input.GetKey(KeyCode.Joystick2Button4);
-            P2_RB = Input.GetKey(KeyCode.Joystick2Button5);
-            P2_A = Input.GetKey(KeyCode.Joystick2Button0);
-            P2_B = Input.GetKey(KeyCode.Joystick2Button1);
-            P2_X = Input.GetKey(KeyCode.Joystick2Button2);
-            P2_Y = Input.GetKey(KeyCode.Joystick2Button3);
-            P2_Start = Input.GetKey(KeyCode.Joystick2Button7);
         }
 
         if(isKeyboardAndMouseP1)

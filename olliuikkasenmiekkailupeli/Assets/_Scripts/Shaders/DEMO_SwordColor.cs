415c1567173c111326a9b25bc9427b84203591f2
RiikkaKilp <32130006+RiikkaKilp@users.noreply.github.com>
2018-07-23 10:22:44 +0300

    SwordPosition shader demo

---
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/Shaders/DEMO_SwordColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEMO_SwordColor : MonoBehaviour {
+
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Sabreurs/SwordPosition");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("up"))
+        {
+            Debug.Log("Inner Position");
+            rend.material.SetFloat("_ColorScale", 0.0f);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            Debug.Log("Outer Position");
+            rend.material.SetFloat("_ColorScale", 1.0f);
+        }
+    }
+}

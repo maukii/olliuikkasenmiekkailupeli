16ae68f932786790b3c57ff74715caca13ee7fc0
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2018-07-26 15:47:32 +0300

    GameScene shadows + SkyboxRotator

---
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/SkyboxRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotator : MonoBehaviour
+{
+    public float speedMultiplier;
+
+    void Start()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", 0);
+    }
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speedMultiplier);
+    }
+}

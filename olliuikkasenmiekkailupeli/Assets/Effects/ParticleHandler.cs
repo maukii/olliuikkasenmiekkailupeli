74755a06c9a5279e6e8952c5dae828389a809e94
RiikkaKilp <32130006+RiikkaKilp@users.noreply.github.com>
2018-07-27 13:34:52 +0300

    more particle stuff, also effect handler thingie

---
+++ olliuikkasenmiekkailupeli/Assets/Effects/ParticleHandler.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleHandler : MonoBehaviour {
+
+    // This is a script for handling the particles and trails
+    // So sword-swoosh-trail, sword-on-sword -spark, and sword-on-player- blood.
+    // Dunno how the sword-on-sword and sword-on-player collisions should be checked, since
+    // they need to know the collision's position ¯\_(ツ)_/¯
+
+    // How to use:
+    // Put this script on the player object
+    // Put the particle systems/trails that are always on the same place (pipe smoke, sword swoosh)
+    // where they belong (child of object "terä", pipe, etc) and drag them on this script.
+    // The other psystemt/trail, just drag them from the assets folder on this script u know 
+    // this stuff why am I even telling this.
+
+    // --- Current particle systems and trails explained: ---
+    // trailSwoosh = When sword is in "hitting" mode.
+    // partSpark = When sword collides with other sword.
+    // partBlood = When sword collides with player
+
+    public ParticleSystem partSpark, partBlood;
+    public TrailRenderer trailSwoosh;
+    HandAnimationControl hcon;
+
+    ParticleSystem.EmissionModule bloodEmi, sparkEmi;
+    float swooshDefaultTime;
+
+    void Start ()
+    {
+        hcon = GetComponent<HandAnimationControl>();
+
+        // --- STUFF FOR trailSwoosh --- //
+        swooshDefaultTime = trailSwoosh.time;           //Gets the default lifetime of the trail
+        trailSwoosh.time = 0;                           //Sets the current lifetime to zero
+
+        // ---- STUFF FOR partBlood --- //
+        bloodEmi = partBlood.emission;
+        //bloodEmi.rate = new ParticleSystem.MinMaxCurve(0.0f, 10.0f);
+
+    }
+	
+	void Update ()
+    {
+        #region SwooshCheck
+        // --- Check if trailSwoosh should be played --- //
+        if(hcon.swordSwinging == true)
+        {
+            if (trailSwoosh.time == 0)
+            {
+                trailSwoosh.time = swooshDefaultTime;
+            }
+        }
+
+        if (trailSwoosh.time == swooshDefaultTime)
+        {
+            if (hcon.swordSwinging == false)
+            {
+                trailSwoosh.time = 0;
+            }
+        }
+
+        #endregion
+
+        #region ContactCheck
+        // --- Check if partBlood should be instantiated --- //
+
+        // instantiate blood effect on collision.contactpoint[0] (LASSI'S COLLISION SCRIPT)
+        // take the direction of the normals and align the rotation of the particle system to that
+        // Destroy the instance of the prefab once it's done i has done the sparkly thing
+        // if the collision is sword-on-sword = spark
+        // if sword-on-player = blood
+        #endregion
+    }
+
+}

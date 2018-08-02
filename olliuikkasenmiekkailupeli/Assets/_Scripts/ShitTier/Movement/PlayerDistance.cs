af8b749d8c66ca08a3c55e4e47b5c0788bcf5089
maukii <31925951+maukii@users.noreply.github.com>
2018-08-02 14:01:03 +0300

    groundwork to distance checking

---
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/ShitTier/Movement/PlayerDistance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDistance : MonoBehaviour
+{
+    CameraScript cs;
+    public float distance;
+    float maxDistance = 10f;
+
+    void Start()
+    {
+        cs = FindObjectOfType<CameraScript>();
+    }
+
+    void Update()
+    {
+        CanBackup();
+    }
+
+    public bool CanBackup()
+    {
+        distance = Vector3.Distance(cs.P1.position, cs.P2.position);
+
+        if(distance >= maxDistance)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
---
+++ olliuikkasenmiekkailupeli/Assets/_Scripts/ShitTier/Movement/WallDistance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDistance : MonoBehaviour
+{
+
+    CameraScript cs;
+
+    [SerializeField] GameObject wallL, wallR;
+    [SerializeField] Transform P1, P2;
+
+    void Start()
+    {
+        cs = FindObjectOfType<CameraScript>();
+    }
+
+    void Update()
+    {
+        GetPlayerPosition();
+        DistanceToBackWall();
+    }
+
+    private void DistanceToBackWall()
+    {
+        
+    }
+
+    private void GetPlayerPosition()
+    {
+        P1 = cs.P1;
+        P2 = cs.P2;
+    }
+}
